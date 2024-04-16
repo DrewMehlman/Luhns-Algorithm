c32bd652a0aaf6df29c95d68ddd42238296df420
Andrew Mehlman <mehlmadm@mail.uc.edu>
2024-04-16 12:08:39 -0400

    cc

---
+++ Luhns Algorithm/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luhns_Algorithm
+{
+    internal class Class1
+    {
+        public class ValidateCard(String cardNumber)
+        {
+            cardNumber = "4532835088732449";
+        }
+    }
+}
